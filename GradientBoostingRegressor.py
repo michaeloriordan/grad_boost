f04603c21d4e15322b290359be82e0ed14fc8bbf
michael <michaeloriordan4@gmail.com>
2020-03-10 18:05:26 +0000

    Renamed

--- GradientBoostingRegressor.py
+++ GradientBoostingRegressor.py
@@ -2,19 +2,19 @@ import numpy as np
 from DecisionTreeRegressor import DecisionTreeRegressor
 
 
-class BaseEstimator:
+class MeanBaseEstimator:
     def __init__(self):
-        self.base_prob = 0
+        self.y_mean = 0
 
     def fit(self, X, y):
-        self.base_prob = np.mean(y)
+        self.y_mean = np.mean(y)
         return self
 
     def predict(self, X):
-        return np.ones(X.shape[0]) * self.base_prob
+        return np.ones(X.shape[0]) * self.y_mean
 
 
-class GradientBoostingClassifier:
+class GradientBoostingRegressor:
     def __init__(self, n_estimators=100, max_depth=3, max_features='sqrt',
                  min_samples_split=2, min_samples_leaf=1, learning_rate=0.1,
                  max_split_values=100):
@@ -27,7 +27,7 @@ class GradientBoostingClassifier:
         self.max_split_values = max_split_values
 
     def fit(self, X, y):
-        self.base = BaseEstimator().fit(X, y)
+        self.base = MeanBaseEstimator().fit(X, y)
         self.trees = []
 
         y_pred = self.base.predict(X)
